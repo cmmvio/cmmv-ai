1a85aea429a5bc949f5a660feb612dae351a9ae0
Andre <andre@tos.world>
2025-02-22 20:39:14 -0300

    fix: bedrock llm settings

--- src/llms/bedrock.llm.ts
+++ src/llms/bedrock.llm.ts
@@ -6,9 +6,16 @@ export class BedrockLLM extends AbstractLLM {
 
     public override async initialize() {
         const { ChatBedrockConverse } = await import('@langchain/aws');
+        const model = Config.get(
+            'ai.llm.model',
+            'anthropic.claude-3-5-sonnet-20240620-v1:0',
+        );
+        const AWSSettings = Config.get('ai.aws', {});
         const options = Config.get('ai.llm', {});
 
         this.llm = new ChatBedrockConverse({
+            model,
+            ...AWSSettings,
             ...options,
         });
 
